0b63035f09e1e53cff7873247a5eb25f227c4bdc
Luis Hernandez <luisernestohernandez1@hotmail.com>
2023-09-25 09:42:01 -0700

    Agregue a los integrantes del equipo
    Agregue los espectaculares nombres de los integrantes de este magnifico equipo

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("Hola mundo");
 console.log("Luis Ernesto Hernandez Lopez");
+console.log("Ilse Espinoza Flores");
+console.log("Karla Arleth Lerma Molina");
+console.log("Jorge Luis Lopez Quintana");
+console.log("Joan Andres Kniffin Ortiz");
+console.log("Alejandra Quirarte Aguero");
+
 console.log("Practica 1");
